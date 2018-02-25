7503615b9a026f1395af090343e684306f408cdf
jackS <jastajas@gamil.com>
2018-02-25 22:12:41 +0100

    Klasa Filmueb - ver 0.3

--- src/com/example/filmueb/app/Filmueb.java
+++ src/com/example/filmueb/app/Filmueb.java
@@ -3,16 +3,65 @@ import com.example.filmueb.model.Actor;
 import com.example.filmueb.model.Film;
 import com.example.filmueb.model.Series;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Filmueb {
     public static void main(String[] args) {
 
-        System.out.println("Application ver. 0.2");
+        System.out.println("Application ver. 0.3");
+        Scanner sc = new Scanner(System.in);
 
-        ConsoleDataReader cdr = new ConsoleDataReader(new Scanner(System.in));
+        ArrayList<Film> films = new ArrayList<>();
+        ArrayList<Actor> actors = new ArrayList<>();
+        ArrayList<Series> series = new ArrayList<>();
 
-        try{
+
+        Option[] options = Option.values();
+
+        int a = 0;
+        do {
+            System.out.println("Proszę wybrać nr opcji: ");
+            for (int i = 0; i < options.length; i++) {
+                System.out.println(options[i].toString());
+            }
+            System.out.println("Nr opcji: ");
+            try {
+                switch (a = sc.nextInt()) {
+                    case 0:
+                        films.add(ConsoleDataReader.createMovie());
+                        break;
+                    case 1:
+                        actors.add(ConsoleDataReader.createActor());
+                        break;
+                    case 2:
+                        series.add(ConsoleDataReader.createTvSeries());
+                        break;
+                    case 3:
+                        System.out.println(films.toString());
+                        break;
+                    case 4:
+                        System.out.println(actors.toString());
+                        break;
+                    case 5:
+                        System.out.println(series.toString());
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        System.out.println("Nie ma takiego nr opcji.");
+                        a = 0;
+                }
+            } catch (Exception e) {
+                System.out.println("Wprowadzono błędne dane.");
+                a = 0;
+                sc.nextLine();
+            }
+
+        }while (!options[a].name().equals("END_PROGRAM"));
+
+        System.out.println("Działanie programu dobiegło końca.");
+        /*try{
             Film film1 = cdr.createMovie();
             System.out.println(film1.toString());
         }
@@ -34,7 +83,7 @@ public class Filmueb {
         }
         catch (NullPointerException n){
             System.out.println("Brak danych lub podano nieprawidłowe informacje.");
-        }
+        }*/
 
 
         /*Film film1 = new Film("Lucy", "Luc Besson", 2014, "Sci-Fi", "Podczas przemytu narkotyków w swoim organizmie Lucy przypadkowo zyskuje nadprzyrodzone zdolności.", 6.3);
